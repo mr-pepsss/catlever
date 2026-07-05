b5a0642670a44df6e438d601fa0d754b95a5afc5
Eugene Koval <eugenekoval@MacBook-Pro-Eugene.local>
2026-07-05 15:20:15 -0600

    catlever: initial one-page site

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const CONFIG = {
   ticker: 'CATLEVER',
   chain: 'solana',
-  contractAddress: 'n5crU8r3Z9qCFD2ZzEcPjuJQd3utYpEVorTKta2pump',
-  buyUrl: 'https://pump.fun/coin/n5crU8r3Z9qCFD2ZzEcPjuJQd3utYpEVorTKta2pump',
+  contractAddress: 'e6zaz4JNn8qBHE7HDZm39uTNx7ATs5kCjVQRi2Cpump',
+  buyUrl: 'https://pump.fun/coin/e6zaz4JNn8qBHE7HDZm39uTNx7ATs5kCjVQRi2Cpump',
   socials: {
     x: 'https://x.com/Catleverfun'
   }
